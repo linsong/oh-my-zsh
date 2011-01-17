e828b8d5144ade902d2a86cee57fe94a8c8df6c6
Vincent Wang <linsong.qizi@gmail.com>
2011-01-17 20:04:18 +0800

    disable precmd and preexec

--- lib/completion.zsh
+++ lib/completion.zsh
@@ -62,3 +62,26 @@ zstyle ':completion:*:*:*:users' ignored-patterns \
 # ... unless we really want to.
 zstyle '*' single-ignored show
 
+#autoload bashcompinit
+#bashcompinit
+#
+#bash_source() {
+#  alias shopt=':'
+#  alias _expand=_bash_expand
+#  alias _complete=_bash_comp
+#  emulate -L sh
+#  setopt kshglob noshglob braceexpand
+#
+#  source "$@"
+#}
+#
+#have() {
+#  unset have
+#  (( ${+commands[$1]} )) && have=yes
+#}
+
+
+#for i in ~/.my_bash/completion/* ; do 
+  #bash_source $i 
+#done
+
