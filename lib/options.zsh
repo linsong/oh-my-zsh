72f6dd38a2a19512795a38cab94d1a5dce5f896c
Vincent Wang <linsong.qizi@gmail.com>
2011-01-21 22:07:25 +0800

    customize options

--- lib/options.zsh
+++ lib/options.zsh
@@ -1,4 +1,9 @@
 setopt interactive_comments  # allow commenting out a cmd interactively
+#setopt rematch_pcre
+setopt cdable_vars
+setopt extended_glob
+#setopt ksh_glob              # allow *(...), !(...) +(...), ?(...) etc
+                              # e.g.: ls (^(arch*)) match all files not start with arch
 
 zle_highlight=(region:bg=green special:standout isearch:fg=red,underline)
 
